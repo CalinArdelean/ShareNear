f4c8ebe36f086a0f047034942976d936afb41b51
areebarizvi <areeba.rizvi@mail.utoronto.ca>
2019-10-18 18:51:10 -0400

    edited mainpage

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,26 +3,13 @@ import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 //import Button from 'react-bootstrap/Button';
-import Button from '@material-ui/core/Button';
+//import Button from '@material-ui/core/Button';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Button> Hello </Button>
-        <Button> Hi </Button>
-        <p>
-          Edit <code>HI</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={logo} className="App-logo" alt="logo" />      
       </header>
     </div>
   );

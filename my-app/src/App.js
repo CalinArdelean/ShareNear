c32c06ebac589506f4ba4b0e92e7f1ccfa81a7c0
usmansha <shahmir.usman@mail.utoronto.ca>
2019-10-18 20:06:05 -0400

    Added navbar

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,16 +3,43 @@ import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 //import Button from 'react-bootstrap/Button';
-//import Button from '@material-ui/core/Button';
+import Button from '@material-ui/core/Button';
+import { Navbar, Nav, Form, FormControl, Container, Row, Col} from 'react-bootstrap';
+
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />      
+
+      <Container>
+        <Row>
+          <Col>  
+            <Navbar bg="dark" variant="dark" className="App-navbar">
+              <Navbar.Brand href="#home"><img src={logo} className="App-logo" alt="logo" />  </Navbar.Brand>
+                <Nav className="mr-auto">
+                  <Nav.Link href="#profile">View Profile</Nav.Link>
+                </Nav>
+                <Form inline>
+                  <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+                  <Button variant="outline-info">Search</Button>
+                </Form>
+            </Navbar>
+          </Col>
+        </Row>
+      </Container>
+
+
+      
+
+
+            
       </header>
     </div>
   );
 }
+
+
 
 export default App;

41cefef7e05aa11c143d0566c1c3f949ae58650c
taniapereira089 <taniapereira089@gmail.com>
2019-10-24 02:34:12 -0400

    edited login page

--- my-app/src/login.js
+++ my-app/src/login.js
@@ -1,35 +1,45 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './App.css';
+import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/core/styles';
 
 /* Component for the Home page */
+
 class login extends React.Component {
     render() {
         return (
             <div>
 
-                { /* Inputs to add student */}
+                { /* Inputs to add user */}
+                <br></br>
+                <br></br>
+                <h4>Welcome to ShareNear</h4>
+                <br></br>
+                <br></br>
                 <input username='username'
                     //  value={ this.props.studentName } 
                     //  onChange={this.props.handleChange} 
                     type="text"
                     placeholder="username" />
-
+                <br></br>
+                <br></br>
                 <input password='password'
                     //  value={ this.props.studentName } 
                     //  onChange={this.props.handleChange} 
                     type="text"
                     placeholder="password" />
+                <br></br>
+                <br></br>
+                <a href="./">Don't have an account already? Sign Up Here</a>
 
-
-
-
-
-
-
-                {/* <button onClick={ this.props.addStudent }>Add Student</button> */}
+                {/* <Button variant="contained" className={classes.button}>
+                    Default
+                </Button>
+                 */}
 
             </div>
+
         );
     }
 }

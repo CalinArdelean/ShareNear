f32e7ad2d822a5f9599d9600a03516c5deac902f
Calin Ardelean <calin.a@hotmail.com>
2019-10-23 21:11:40 -0400

    Added HomeLayout in App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,35 +4,35 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 //import Button from 'react-bootstrap/Button';
 import Button from '@material-ui/core/Button';
-import {Route, Switch, BrowserRouter} from 'react-router-dom';
-import { Navbar, Nav, Form, FormControl, Container, Row, Col} from 'react-bootstrap';
+import { Route, Switch, BrowserRouter } from 'react-router-dom';
+import { Navbar, Nav, Form, FormControl, Container, Row, Col } from 'react-bootstrap';
 import AppNavbar from './Navbar';
 import login from './login';
+import ComplexGrid from './components/HomeLayout';
 
 
 class App extends React.Component {
-  render() {
-  return (
+    render() {
+        return (
 
-    <div className="App">
-      <header className="App-header">
-        <AppNavbar/>
-      </header>
+            <div className="App">
+                <header className="App-header">
+                    <AppNavbar />
+                </header>
 
-      <div>
-        <BrowserRouter>
-          <Switch> { /* Similar to a switch statement - shows the component depending on the URL path */ }
-            { /* Each Route below shows a different component depending on the exact path in the URL  */ }
-            <Route exact path='/login' component={login}/>
-          </Switch>
-        </BrowserRouter>
-      </div>
+                <div>
+                    <BrowserRouter>
+                        <Switch> { /* Similar to a switch statement - shows the component depending on the URL path */}
+                            { /* Each Route below shows a different component depending on the exact path in the URL  */}
+                            <Route exact path='/login' component={login} />
+                        </Switch>
+                    </BrowserRouter>
+                </div>
+                <ComplexGrid />
 
-    </div>
-  );
-  }
+            </div>
+        );
+    }
 }
-
-
 
 export default App;

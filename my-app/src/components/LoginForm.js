4654a2cabd6200007398b88a057a2f1d985939a2
AreebaRizvi <areebar247@gmail.com>
2019-11-02 18:00:38 -0400

    got the login functionality to work properly (edited)

--- my-app/src/components/LoginForm.js
+++ my-app/src/components/LoginForm.js
@@ -20,51 +20,6 @@ class LoginForm extends React.Component {
          
         </div>
       );
-      // return (
-      //   <div className="Form">
-      //     <br></br>
-      //     <br></br>
-      //     <a href = "./">
-      //     <img
-      //           alt="ShareNear Logo"
-      //           src={logo}
-      //           width="175px"
-      //           height="100px"
-      //           className="d-inline-block align-top"
-      //     />
-      //     </a>
-      //     <br></br>
-      //     <h3>Welcome to ShareNear</h3>
-      //     <p>Your One-Stop Online Rent Marketplace</p>
-      //     <br></br>
-      //     <input name='userName' 
-      //            value={ this.props.userName } 
-      //            onChange={this.props.handleChange} 
-      //            type="text" 
-      //            placeholder="username" />
-  
-      //     <br></br>
-      //     <br></br>
-  
-      //     <input name='userPassword' 
-      //            value={ this.props.userPassword } 
-      //            onChange={this.props.handleChange} 
-      //            type="text" 
-      //            placeholder="password" />
-  
-      //     <br></br>
-      //     <br></br>
-      
-      //     <a href="./">
-      //       <button onClick={this.props.checkUser}>Sign In</button>
-      //    </a>
-      //     <br></br>
-      //     <br></br>
-      //     <a href="./signup">Don't have an account already? Sign Up Here</a>
-  
-      //   </div>
-  
-      // );
     }
     else if(this.props.validUser === 0){
     return (

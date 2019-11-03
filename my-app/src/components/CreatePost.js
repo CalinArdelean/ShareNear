8b527941291578ac6974b46cf951107611a4933c
taniapereira089 <taniapereira089@gmail.com>
2019-11-03 15:48:58 -0500

    saving new item listing

--- my-app/src/components/CreatePost.js
+++ my-app/src/components/CreatePost.js
@@ -1,10 +1,33 @@
 import React from 'react';
 import '../App.css';
-
+import ItemPic from '../assets/NewItem.png'
 import AppNavbarLoggedIn from './NavbarLoggedIn';
 
 /* Component for a user to create a post and put an item up for rent */
 class newPost extends React.Component {
+    
+    newItem = {
+        id: 9,
+        image: ItemPic,
+        name: "",
+        description: "",
+        price: "",
+        location: "",
+        seller: "John Doe",
+        address: "123 Placeholder Lane"
+    }
+
+    handleInputChange = (event) => {
+        const target = event.target
+        const value = target.value
+        const name = target.name
+
+        this.setState({
+            [name]: value
+        })
+
+    }
+
     render() {
         return (
             <div className="Edit">
@@ -12,10 +35,10 @@ class newPost extends React.Component {
 
                 <div id="sc-edprofile">
                     <h1>Post a New Item for Rent</h1>
-                    <div class="sc-container">
-                        <input type="text" placeholder="Name of Item" />
-                        <textarea placeholder="Description of Item" />
-                        <input type="text" placeholder="Price" />
+                    <div className="sc-container">
+                        <input name='name' value={this.newItem.name} onChange={this.newItem.handleChange} type="text" placeholder="Name of Item" />
+                        <textarea name='description' value={this.newItem.description} onChange={this.newItem.handleChange} type="text" placeholder="Description of Item" />
+                        <input name='price' value={this.newItem.price} onChange={this.newItem.handleChange} type="text" placeholder="Price" />
                         <select>
                             <option value="">per hour</option>
                             <option value="">per day</option>
@@ -23,8 +46,9 @@ class newPost extends React.Component {
                             <option value="">per month</option>
                             <option value="">other</option>
                         </select>
-                        <input type="text" placeholder="Location" />
+                        <input name='location' value={this.newItem.location} onChange={this.newItem.handleChange} type="text" placeholder="Location" />
                         <button>Upload Item Picture</button>
+                        console.log(this.newItem);
                         <a href="./"><input type="submit" value="Create Post" /></a>
                     </div>
                 </div>
--- my-app/src/components/UserProfile.js
+++ my-app/src/components/UserProfile.js
@@ -5,16 +5,13 @@ import PaulBlart from '../assets/paul_blart.jpg';
 import MarySmith from '../assets/adminpicture.jpg';
 import JacobJacobson from '../assets/louisCK.jpg';
 import AppNavbarLoggedIn from './NavbarLoggedIn';
-
+//import { user } from './LoginPage';
 
 var sellers = [
-    {
-        id: 0, name: 'John Doe', address: 'North York, ON', description: 'Hi I\'m John! I primarily rent out household items that I no longer have much use for. If you are interested in one of my listings, have questions or concerns, or would like to negotiate prices, feel free to contact me, preferably through my cell.Thanks in advance!',
-        phoneNumber: '416-900-8555', email: 'JohnDoe123@hotmail.com', itemIDs: [], image: profilepicture
-    },
+    { id: 0, name: 'John Doe', address: 'North York, ON', description: 'Hi I\'m John! I primarily rent out household items that I no longer have much use for. If you are interested in one of my listings, have questions or concerns, or would like to negotiate prices, feel free to contact me, preferably through my cell.Thanks in advance!', phoneNumber: '416-900-8555', email: 'JohnDoe123@hotmail.com', itemIDs: [], image: profilepicture },
     { id: 1, name: 'Paul Blart', address: 'Mississauga, ON', description: 'Hi, I\'m Paul. Hope you enjoy my stuff! Phone number below.', phoneNumber: '343-373-5121', email: 'paulsstuff@gmail.com', itemIDs: [0, 2, 3, 5], image: PaulBlart },
     { id: 2, name: 'Jacob Jacobson', address: 'Toronto, ON', description: 'Hey, welcome to my profile. Feel free to call me any time of the day!', phoneNumber: '416-647-7644', email: 'jacobjacob@gmail.com', itemIDs: [1, 4, 6, 7, 8], image: JacobJacobson },
-    { id: 3, name: 'Mary Smith', address: 'Oakville, ON', description: 'Proud to see ShareNear being used for frequently. Questions about the site? Please send me an email!', phoneNumber: '647-880-3325', email: 'MarySmithers@hotmail.com', itemIDs: [], image: MarySmith }
+    { id: 3, name: 'Mary Smith', address: 'Oakville, ON', description: 'Proud to see ShareNear being used so frequently. Questions about the site? Please send me an email!', phoneNumber: '647-880-3325', email: 'MarySmithers@hotmail.com', itemIDs: [], image: MarySmith }
 ]
 
 
@@ -25,7 +22,7 @@ class Profile extends React.Component {
     
 
     render() {
-
+        //console.log(user);
         let data = this.props.location.data;
         this.props.location.state = this.props.location.data;
         console.log(this.props.location.state);
@@ -57,7 +54,7 @@ class Profile extends React.Component {
                     <h4>{seller.name}</h4>
                     <br/>
                     <img
-                        alt=""
+                        alt="profile picture"
                         src={seller.image}
                         className="d-inline-block align-top"
                     ></img>
@@ -80,7 +77,7 @@ class Profile extends React.Component {
         if (typeof (data) == 'undefined') {
             return (
                 <div>
-                    <h3>Welcome to your profile John! </h3>
+                    <h3>Welcome to your profile! </h3>
                     <br></br>
                     <div className="Right" className="edit">
                         <a href="./edit">

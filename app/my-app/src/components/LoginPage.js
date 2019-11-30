e98782df86f583144531b31565cf904ca72fea2a
AreebaRizvi <areebar247@gmail.com>
2019-11-30 13:30:51 -0500

    bcrypt

--- app/my-app/src/components/LoginPage.js
+++ app/my-app/src/components/LoginPage.js
@@ -2,19 +2,18 @@ import React, { useState } from "react";
 
 // Importing components
 import LoginForm from './LoginForm';
-import { bool } from 'prop-types';
 import { getState, setState } from "statezero";
+const bcrypt = require('bcryptjs')
 
-var user = {};
 
 export const updateLoginForm = field => {
     const { name, value } = field;
     setState(`loginForm.${name}`, value);
 };
-
+var user = {};
 //component for the login page
 class LoginPage extends React.Component {
-
+  
   ///  React 'state'.  
   // Allows us to keep track of changing data in this component.
   state = {
@@ -31,95 +30,58 @@ class LoginPage extends React.Component {
        email: "MarySmithers@hotmail.com", location: "Oakville, ON"}
     ]
   }
-  // Generic handler for whenever we type in an input box.
-  // We change the state for the particular property bound to the textbox from the event.
-  /*handleInputChange = (event) => {
-    const target = event.target
-    const value = target.value
-    const name = target.name
-    
-    this.setState({
-      [name]: value  
-    })
-
-	//const { name, value } = event.target;
-    setState(`loginForm.${name}`, value);
-
-  }*/
-
-  
 
   // Function to check if user is valid.  
   // Does not need parameters since it only uses and changes the state.
   checkUser = () => {
+    console.log("checking user")     
 
+    const url = '/users';
 
-		//const [screen, setScreen] = useState("noAuth");
-		//const [username, setUsername] = useState();
-		//const [password, setPassword] = useState();
-		console.log("test");
-		//const auth = () => {
-		// Create our request constructor with all the parameters we need
-		const request = new Request("/students", {
-			method: "post",
-			body: JSON.stringify(getState("loginForm")),
-			headers: {
-			Accept: "application/json, text/plain, */*",
-			"Content-Type": "application/json"
-			}
-		});
-		
-		// Send the request with fetch()
-		fetch(request)
-			.then(res => {
-			if (res.status === 200) {
-				console.log("YOO WE DID IT");
-				return res.json();
-			}
-			})
-			//.then(json => {
-			//if (json.screen !== undefined) {
-			//	console.log("why");
-			//	setScreen(json.screen);
-			//}
-			//})
-			.catch(error => {
-			console.log("We have failed");
-			console.log(error);
-			});
-		//};
+  // Since this is a GET request, simply call fetch on the URL
+  fetch(url)
+  .then((res) => { 
+      if (res.status === 200) {
+          // return a promise that resolves with the JSON body
+         return res.json() 
+     } else {
+          alert('Could not get users')
+     }                
+  })
+  .then((json) => {  // the resolved promise with the JSON body
+      // console.log(json.users)
+      let allUsers = json.users
+      const inputEmail = JSON.stringify(getState("loginForm").email)
+      const inputPassword = JSON.stringify(getState("loginForm").password)
+      // let hashedPassword
+      // bcrypt.genSalt(10, (err, salt) => {
+      //   bcrypt.hash(inputPassword, salt, (err, hash) => {
+      //     console.log(hash)
+      //     hashedPassword = hash
+      //     console.log(JSON.stringify(hashedPassword))
+      //   }) 
+      // })
+      // console.log(JSON.stringify(hashedPassword) === JSON.stringify(allUsers[3].password))
+      console.log(inputPassword)
+      // console.log(JSON.stringify(allUsers[3].password))
+      const dbPassword = JSON.stringify(allUsers[3].password)
+      console.log(dbPassword)
+      bcrypt.compare(inputPassword, dbPassword, (err, res) => {  console.log(inputPassword, dbPassword,'Bcripy result:', res);})
+      // bcrypt.compare("12345678!", "$2a$10$jnVglJC0Lg08N83sd2KfGOOuiElPNDwcmwVjgx4JCGr.fpybJXRda", (err, res) => {  console.log('Bcripy result:', res);})
 
+      // const filteredUsers = allUsers.filter((user) => user.email === inputEmail && user.password === inputPassword )
+      // if(filteredUsers.length === 0){
+      //   this.validUser === 0
+      //   console.log("Valid User")
+      // }
+      // else{
+      //   this.validUser === 1
+      //   console.log("Invalid User")
+      // }
+  }).catch((error) => {
+      console.log(error)
+  })
 
-
-    console.log("checking user")
-
-      /*const userList = this.state.users
-      const username = this.state.userName
-      const pass = this.state.userPassword
-      let valid = 0
-      let id = -1
-      let usernum = 0;
-
-      for(let i = 0; i < userList.length; i++){
-          if(userList[i].user === username && userList[i].pass === pass){
-            valid = 1;
-            id = i;
-            if (i === 1) {
-                usernum = 1;
-            }
-          }
-      }
-      console.log(valid)
-      user = this.state.users[id];
-      console.log(user);
-      console.log(usernum)
-      this.setState({
-        validUser: valid,
-        currentUser: user,
-        admin: usernum
-      });*/
-      
-      
   }
 
   render() {

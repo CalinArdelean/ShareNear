0df5941c700e83e6b4db35edc64fed9fa8bb2ba8
ShahmirU <shahmir.usman@mail.utoronto.ca>
2019-12-02 23:10:31 -0500

    Made User List Page

--- my-app/src/components/UserListPage.js
+++ my-app/src/components/UserListPage.js
@@ -1,44 +1,59 @@
 import React from 'react';
 import '../App.css';
-import AppNavbarLoggedIn from './NavbarLoggedIn';
-import { Table } from 'react-bootstrap';
-import NavbarCollapse from 'react-bootstrap/NavbarCollapse';
 import Navbar from './Navbar';
+import {BootstrapTable, TableHeaderColumn} from 'react-bootstrap-table';
+import _ from 'lodash';
+import 'react-bootstrap-table/dist/react-bootstrap-table.min.css';
+import 'react-bootstrap-table2-filter/dist/react-bootstrap-table2-filter.min.css';
 
 /* Component for an admin to view the list of users (currently hardcoded, but will need to read from a database) */
 class userList extends React.Component {
-    render() {
-        return (
+    constructor() {
+        super();
+        this.state = {
+            users: []
+        };
+    }
 
+    render() {
+        const url = '/users';
+    
+      // Since this is a GET request, simply call fetch on the URL
+      fetch(url)
+      .then((res) => { 
+          if (res.status === 200) {
+              // return a promise that resolves with the JSON body
+             return res.json() 
+         } else {
+              alert('Could not get users')
+         }                
+      })
+      .then((json) => {  // the resolved promise with the JSON body
+          this.setState({users: json.users});
+      }).catch((error) => {
+          console.log(error)
+      });
+
+        return (
+            // {this.loadUsers}
             <div className="UserList">
                 <Navbar />
                 
-                <Table striped bordered hover>
-                    <thead>
-                        <tr>
-                            <th>id</th>
-                            <th>First Name</th>
-                            <th>Last Name</th>
-                            <th>Email</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>1</td>
-                            <td>John</td>
-                            <td>Doe</td>
-                            <td>JohnDoe123@hotmail.com</td>
-                        </tr>
-                        <tr>
-                            <td>2</td>
-                            <td>Mary</td>
-                            <td>Smith</td>
-                            <td>MarySmithers@hotmail.com</td>
-                        </tr>
-                    </tbody>
-                </Table>
-
-
+                <BootstrapTable
+                    data={this.state.users}
+                    search={true}
+                    pagination
+                    striped
+                    hover
+                    condensed
+                >
+                    <TableHeaderColumn dataField="_id" isKey dataAlign="center">ID</TableHeaderColumn>
+                    <TableHeaderColumn dataField="firstname" dataAlign="center">First Name</TableHeaderColumn>
+                    <TableHeaderColumn dataField="lastname" dataAlign="center">Last Name</TableHeaderColumn>
+                    <TableHeaderColumn dataField="username" dataAlign="center">Username</TableHeaderColumn>
+                    <TableHeaderColumn dataField="email" dataAlign="center">Email</TableHeaderColumn>
+                    <TableHeaderColumn dataField="usertype" dataAlign="center">Admin?</TableHeaderColumn>
+                </BootstrapTable>
             </div>
 
         );

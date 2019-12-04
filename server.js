a16880c58f141af824e7120e412c8999128fa828
ShahmirU <shahmirusman1999@gmail.com>
2019-12-03 21:27:43 -0500

    Fixed server

--- server.js
+++ server.js
@@ -330,27 +330,27 @@ app.delete('/users/:id', (req, res) => {
 	})
 })
 
-app.get("/:filename", (req, res) => {
-	gfs.files.findOne({ filename: req.params.filename }, (err, file) => {
-	  // Check if file
-	  if (!file || file.length === 0) {
-		return res.status(404).json({
-		  err: "No file exists"
-		});
-	  }
+// app.get("/:filename", (req, res) => {
+// 	gfs.files.findOne({ filename: req.params.filename }, (err, file) => {
+// 	  // Check if file
+// 	  if (!file || file.length === 0) {
+// 		return res.status(404).json({
+// 		  err: "No file exists"
+// 		});
+// 	  }
   
-	  // Check if image
-	  if (file.contentType === "image/jpeg" || file.contentType === "image/png") {
-		// Read output to browser
-		const readstream = gfs.createReadStream(file.filename);
-		readstream.pipe(res);
-	  } else {
-		res.status(404).json({
-		  err: "Not an image"
-		});
-	  }
-	});
-  });
+// 	  // Check if image
+// 	  if (file.contentType === "image/jpeg" || file.contentType === "image/png") {
+// 		// Read output to browser
+// 		const readstream = gfs.createReadStream(file.filename);
+// 		readstream.pipe(res);
+// 	  } else {
+// 		res.status(404).json({
+// 		  err: "Not an image"
+// 		});
+// 	  }
+// 	});
+//   });
 
 /*** Webpage routes below **********************************/
 // Serve the build
